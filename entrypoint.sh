7204a77af6e9c7686f5018ac26fff3ee480037df
sky-lai <sky.sqa@pgpg.la>
2022-05-06 16:49:42 +0800

    tfyguhij

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,8 @@ echo $2
 # insert credential
 mkdir ~/.ssh
 mv /known_hosts ~/.ssh/known_hosts
+echo $2 >> ~/.ssh/id_rsa
+chmod u=rw,g=,o= ~/.ssh/id_rsa
 
 echo 'ls ~/.ssh'
 ls ~/.ssh
@@ -19,10 +21,7 @@ ls ~/.ssh
 echo 'ls ~/.ssh/known_hosts'
 ls ~/.ssh/known_hosts
 
-echo $2 >> ~/.ssh/id_rsa
-chmod u=rw,g=,o= ~/.ssh/id_rsa
-
 # clone tests repo
-git clone $1 __test-repo
+# git clone $1 __test-repo
 
-ls
+# ls
