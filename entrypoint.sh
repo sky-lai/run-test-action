e71b3a9ca470de86f598114727254efdcb84dd0a
sky-lai <sky.sqa@pgpg.la>
2022-05-06 13:29:20 +0800

    huinjiomqhtqgt5sw4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,6 @@
 #!/bin/sh -l
-pwd
-echo "ls -al"
-ls -al
 
-echo ""
-echo "ls .. -al"
-ls .. -al
-
-echo ""
-echo "ls ../file_commands -al"
-ls ../file_commands -al
-
-echo ""
-echo "ls ../home -al"
-ls ../home -al
-
-echo ""
-echo "ls ../workflow -al"
-ls ../workflow -al
-
-echo "HOME<$HOME>"
-echo "GITHUB_WORKDIR:<$GITHUB_WORKSPACE>"
-
-mkdir ${GITHUB_WORKSPACE}/tests
-mv /testsabc123/* ${GITHUB_WORKSPACE}/tests
-echo "${GITHUB_WORKSPACE}/tests"
-ls $GITHUB_WORKSPACE
-
-time=$(date)
-echo "::set-output name=time::$time"
+echo "\$0"
+echo $0
+echo "\$1"
+echo $1
