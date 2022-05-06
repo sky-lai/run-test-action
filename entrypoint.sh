11f07fcba5cb207ee183d054abc1396d65ac24b7
sky-lai <sky.sqa@pgpg.la>
2022-05-06 09:14:07 +0800

    test mount right

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/sh -l
 echo $GITHUB_WORKDIR
 
-mkdir ${GITHUB_WORKDIR}/tests
-mv /tests/* ${GITHUB_WORKDIR}/tests
+mv /testsabc123/* /abc123
+echo "/abc123"
+ls /abc123
 
+mkdir /testmkdir
+mv /testsabc123/* /testmkdir
+echo "/testmkdir"
+ls /testmkdir
+
+mkdir /github/testmkdir
+mv /testsabc123/* /github/testmkdir
+echo "/testmkdir"
+ls /github/testmkdir
+
+mkdir ${GITHUB_WORKDIR}/tests
+mv /testsabc123/* ${GITHUB_WORKDIR}/tests
+echo "/testmkdir"
 ls $GITHUB_WORKDIR
 
 time=$(date)
