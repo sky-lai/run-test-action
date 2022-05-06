cd655a68638031b0e44bd62c988b1e700fded4e9
sky-lai <sky.sqa@pgpg.la>
2022-05-06 10:23:29 +0800

    ahahteahtrh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,18 @@ echo ""
 echo "ls .. -al"
 ls .. -al
 
+echo ""
+echo "ls ../file_commands -al"
+ls ../file_commands -al
+
+echo ""
+echo "ls ../home -al"
+ls ../home -al
+
+echo ""
+echo "ls ../workflow -al"
+ls ../workflow -al
+
 echo "HOME<$HOME>"
 echo "GITHUB_WORKDIR:<$GITHUB_WORKSPACE>"
 
