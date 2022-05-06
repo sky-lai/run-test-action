bac55cd2a65a73a81b0986373a5098fb35323808
sky-lai <sky.sqa@pgpg.la>
2022-05-06 09:58:58 +0800

    cytgluih;

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ mv /testsabc123/* /github/testmkdir
 echo "/testmkdir"
 ls /github/testmkdir
 
-mkdir ${GITHUB_WORKDIR}/tests
-mv /testsabc123/* ${GITHUB_WORKDIR}/tests
-echo "/testmkdir"
-ls $GITHUB_WORKDIR
+# mkdir ${GITHUB_WORKDIR}/tests
+# mv /testsabc123/* ${GITHUB_WORKDIR}/tests
+# echo "/testmkdir"
+# ls $GITHUB_WORKDIR
 
 time=$(date)
 echo "::set-output name=time::$time"
