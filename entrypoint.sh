8af2e52f3416dc69303114f589e395fcb0c22de4
sky-lai <sky.sqa@pgpg.la>
2022-05-06 10:01:51 +0800

    d

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,10 @@
 #!/bin/sh -l
 echo $GITHUB_WORKDIR
 
-mkdir /github/testmkdir
-mv /testsabc123/* /github/testmkdir
-echo "/testmkdir"
-ls /github/testmkdir
-
-# mkdir ${GITHUB_WORKDIR}/tests
-# mv /testsabc123/* ${GITHUB_WORKDIR}/tests
-# echo "/testmkdir"
-# ls $GITHUB_WORKDIR
+mkdir ${GITHUB_WORKDIR}/tests
+mv /testsabc123/* ${GITHUB_WORKDIR}/tests
+echo "${GITHUB_WORKDIR}/tests"
+ls $GITHUB_WORKDIR
 
 time=$(date)
 echo "::set-output name=time::$time"
