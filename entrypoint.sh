f4b8cdc8966a575e097351539d829055c985f7c3
sky-lai <sky.sqa@pgpg.la>
2022-05-09 10:56:45 +0800

    egqrgq4fawrgf

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh -l
 
 # insert credential
-mkdir ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
+
+mkdir $HOME/.ssh
 mv ~/.ssh/known_hosts $HOME/.ssh/known_hosts
-echo $2 >> ~/.ssh/id_rsa
-chmod u=rw,g=,o= ~/.ssh/id_rsa
+
+echo $2 >> $HOME/.ssh/id_rsa
+chmod u=rw,g=,o= $HOME/.ssh/id_rsa
 
 echo 'cat $HOME/.ssh/known_hosts'
 cat $HOME/.ssh/known_hosts
