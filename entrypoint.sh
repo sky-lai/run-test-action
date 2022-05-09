54e5077cced6790aeb4fa6f4c4f3af9756bc69d6
sky-lai <sky.sqa@pgpg.la>
2022-05-09 13:14:33 +0800

    qgqgg4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/sh -l
 
 
-echo "\$HOME=$HOME"
+# echo "\$HOME=$HOME"
 
-# Known Hosts
-mkdir $HOME/.ssh
+# # Known Hosts
+# mkdir $HOME/.ssh
 
-ssh-keyscan github.com >> $HOME/.ssh/known_hosts
-ssh-keyscan bitbucket.org >> $HOME/.ssh/known_hosts
+# ssh-keyscan github.com >> $HOME/.ssh/known_hosts
+# ssh-keyscan bitbucket.org >> $HOME/.ssh/known_hosts
 
-# Private Key
-echo $2 >> $HOME/.ssh/id_rsa
-chmod u=rw,g=,o= $HOME/.ssh/id_rsa
+# # Private Key
+# echo $2 >> $HOME/.ssh/id_rsa
+# chmod u=rw,g=,o= $HOME/.ssh/id_rsa
 
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=$HOME/.ssh/known_hosts -o StrictHostKeyChecking=no"
-echo $GIT_SSH_COMMAND
-# clone tests repo
-git clone git@bitbucket.org:sky-lai/a-simple-api-test.git __tests-cases
+# GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=$HOME/.ssh/known_hosts -o StrictHostKeyChecking=no"
+# echo $GIT_SSH_COMMAND
+# # clone tests repo
+# git clone git@bitbucket.org:sky-lai/a-simple-api-test.git __tests-cases
 ls
