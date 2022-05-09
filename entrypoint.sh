b5392048b8a5abf778ae46862eac4359d4ea3e85
sky-lai <sky.sqa@pgpg.la>
2022-05-09 10:55:36 +0800

    sgawegar

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,13 @@
 mkdir ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 ssh-keyscan bitbucket.org >> ~/.ssh/known_hosts
+mv ~/.ssh/known_hosts $HOME/.ssh/known_hosts
 echo $2 >> ~/.ssh/id_rsa
 chmod u=rw,g=,o= ~/.ssh/id_rsa
 
-echo 'cat ~/.ssh/known_hosts'
-cat ~/.ssh/known_hosts
+echo 'cat $HOME/.ssh/known_hosts'
+cat $HOME/.ssh/known_hosts
 
 # clone tests repo
-git clone $1 __tests-cases
+git clone git@github.com:sky-lai/a-simple-api-tests.git __tests-cases
 ls
