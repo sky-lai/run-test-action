5cf78e27f0b067dd6a8cc6e26ffe213b5f7d97b1
sky-lai <sky.sqa@pgpg.la>
2022-05-09 10:44:26 +0800

    sgwegw2ㄑ

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,6 @@ echo 'cat ~/.ssh/known_hosts'
 cat ~/.ssh/known_hosts
 
 # clone tests repo
-git clone git@bitbucket.org:sky-lai/a-simple-api-test.git test-repo
+git clone https://sky-lai:XZjFbAzyPs3g4ZswzH8c@bitbucket.org/sky-lai/a-simple-api-test.git
 
 ls
