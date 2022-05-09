b890d4f0579dbdc5faf5ce1eceacb8653690b99e
sky-lai <sky.sqa@pgpg.la>
2022-05-09 11:22:38 +0800

    qgqgqfqg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh -l
 
 # insert credential
-mkdir $HOME/.ssh
+# mkdir $HOME/.ssh
 
-ssh-keyscan github.com >> $HOME/.ssh/known_hosts
-ssh-keyscan bitbucket.org >> $HOME/.ssh/known_hosts
-chmod 644 $HOME/.ssh/known_hosts
+# ssh-keyscan github.com >> $HOME/.ssh/known_hosts
+# ssh-keyscan bitbucket.org >> $HOME/.ssh/known_hosts
+# chmod 644 $HOME/.ssh/known_hosts
 
-echo $2 >> $HOME/.ssh/id_rsa
-chmod u=rw,g=,o= $HOME/.ssh/id_rsa
+# echo $2 >> $HOME/.ssh/id_rsa
+# chmod u=rw,g=,o= $HOME/.ssh/id_rsa
 
 # clone tests repo
 git clone git@github.com:sky-lai/a-simple-api-tests.git __tests-cases
